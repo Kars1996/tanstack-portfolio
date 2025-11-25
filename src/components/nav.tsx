d50288218e30067f9b97c3715d9efcf15a1d318a
Kars <k@kars.bio>
2025-11-25 00:03:37 +0000

    chore: lower nav z-index for preloader

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -17,7 +17,7 @@ export function Nav() {
   ]
 
   return (
-    <nav className="fixed top-0 w-full z-50 p-6 flex justify-between items-center mix-blend-difference text-white">
+    <nav className="fixed top-0 w-full z-40 p-6 flex justify-between items-center mix-blend-difference text-white">
       <Link
         to="/"
         className="text-2xl uppercase font-display font-bold tracking-tighter hover:opacity-80 transition-opacity"
